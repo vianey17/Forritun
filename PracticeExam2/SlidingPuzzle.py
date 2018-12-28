90153ac4844c0b57f365bdc9705ec3d1d67ea7d3
vianey17 <vianey17@ru.is>
2018-12-28 22:19:39 +0000

    Practice Test 2: Student Grades

---
+++ PracticeExam2/SlidingPuzzle.py
@@ -0,0 +1,30 @@
+DIM = 4
+EMPTYSLOT = 0
+QUIT = 0
+
+def initialize_board():
+    numbers = input().split(" ")
+    numbers = [int(number) for number in numbers]
+    puzzle_board = []
+    index = 0
+    for _ in range(DIM):
+        row = numbers[index:index + DIM]
+        index += DIM
+        puzzle_board.append(row)
+
+    return puzzle_board
+    
+
+def display(puzzle_board):
+    print()
+    for i in range(DIM):
+        for j in range(DIM):
+            if puzzle_board[i][j] == EMPTYSLOT:
+                print("\t", end="")
+            else:
+                print(str(puzzle_board[i][j]) + "\t", end="")
+        print()
+    print()
+
+
+# Your code goes here...
--- PracticeExam2/StudentGrades.py
+++ PracticeExam2/StudentGrades.py
@@ -9,3 +9,75 @@ he student's average grade. The average grade should be printed with 2 digits af
 
 If two or more students have the same average grade the student that appears first in the alphabetical order should be printed.
 """
+
+class Student(object):
+    def __init__(self,name):
+        self.name = name
+        self.grades_list = []
+    
+    def __str__(self):
+        return "{}: {}".format(self.name,self.grades_list)
+    
+    def __lt__(self,other):
+        return self.name < other.name
+    
+    def get_name(self):
+        return self.name
+    
+    def add_grade(self):
+        cardinal = len(self.grades_list) + 1
+        try:
+            grade = float(input("Input grade number {}: ".format(cardinal)))
+            self.grades_list.append(grade)
+            return self.grades_list
+        except:
+            print("Enter digits only")
+            return self.add_grade()
+    
+    def gpa(self):
+        gpa = sum(self.grades_list)/len(self.grades_list)
+        return gpa
+    
+    def highest_grade(self):
+        highest_grade = 0
+        for grade in self.grades_list:
+            if grade > highest_grade:
+                highest_grade = grade
+        return highest_grade
+    
+def print_student_with_highest_grade(students):
+    highest_gpa = 0
+    student_name = ""
+
+    for student in students:
+        if student.gpa() > highest_gpa:
+            highest_gpa = student.gpa()
+            student_name = student.get_name()
+    
+    print("\nStudent with highest average grade:")
+    print("{} has an average grade of {:.2f}".format(student_name,highest_gpa))
+
+
+
+def main():
+    class_size = 4  # Number of students
+    number_of_grades = 3  # How many assignments?
+    students = []  # List will contain student classes
+
+    for i in range(0,class_size):
+        student_name = input("Input student name: ")
+        student = Student(student_name)
+        for j in range(0,number_of_grades):
+            student.add_grade()
+        students.append(student)
+    students.sort()
+    
+    print("Student list")
+    for student in students:
+        print(student)
+    
+    print_student_with_highest_grade(students)
+
+
+
+main()
